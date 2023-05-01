010992c0dc54f44b3acb99092599d2be60be5e85
cjflan <89868659+cjflan@users.noreply.github.com>
2023-05-01 16:59:04 -0500

    add JWT validation and change port

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"log"
+
 	"github.com/cjflan/go-api-scaffold/controllers"
+
 	"github.com/gin-gonic/gin"
+
+	middleware "github.com/cjflan/go-api-scaffold/middleware"
 )
 
 func main() {
@@ -10,9 +15,14 @@ func main() {
 
 	add_routes(router)
 
-	router.Run(":8080")
+	log.Print("Server listening on http://localhost:3000")
+	if err := router.Run(":3000"); err != nil {
+		log.Fatalf("There was an error with the http server: %v", err)
+	}
+
 }
 
 func add_routes(router *gin.Engine) {
 	router.GET("/ping", controllers.Ping)
+	router.GET("/validate", middleware.CheckJWTMiddle(), controllers.Login)
 }
